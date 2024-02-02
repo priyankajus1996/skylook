6457adb0932fae6bf2785552d312f78479bb798f
priyankajus1996 <priyankajus96@gmail.com>
2024-02-02 09:39:27 +0530

    Added style for about section

--- skylook-web/src/components/AboutSection.js
+++ skylook-web/src/components/AboutSection.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'; 
-import Container from 'react-bootstrap/Container'
+import Container from 'react-bootstrap/Container';
+import './AboutSection.css';
 
 function AboutSection() {
   return (
    <>
-     <Container> 
-        <h1>Fluid jumbotron !</h1> 
+     <div className='about-container'>
+      <Container>
+        <h1>ABOUT COMPANY</h1> 
+        <h3>We Are Providing Best Quality Signage products</h3>
         <p> 
-           This is a modified fluid jumbotron which 
-           stretches the whole horizontal space.     
+        Skylook Productions is one of the leading best Sign Board manufacturers in pondicherry and across the all over india delivering the products and We are in this field of 10+ years of expertise and vast network of professionals. This experience giving good quality work to the customer and deliver the products as per the customer needs and expectations. We used the best quality materials to making the products and we are trustable and we have many happy customers.     
         </p> 
-        <Button variant="primary"> 
-         Primary Button 
-        </Button> 
-      </Container> 
+        <p>
+        We are Providing 3D Letter Sign Board, LED with acrylic letter sign board, Vinyl Printing Signage, Glow Sign Boards, Digital Sign Boards,  Sign Boards. we offer high quality led sign boards, Neon sign board led reverse lighting sign, 3d sign board, LED Signage, Shop Name Boards, Architectural Signage, Metal Letter Signage, Glass Signage, Slim Light Boards, Front Light Boards, Back Light Boards, as per the customer needs we are provding.
+        </p>
+        </Container>
+        </div>
    </>
   )
 }
--- skylook-web/src/components/HeroSection.js
+++ skylook-web/src/components/HeroSection.js
@@ -8,13 +8,16 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <div className="hero-text">
       <h1>SkyLook Productions</h1>
       <p>Sign Board & Interiors</p>
       <p>We build Indoor and Outdoor Signage that make businesses.</p>
-       <div className='hero-btns'>
+      <div className='hero-btns'>
         <Button variant="outline-info"> GET STARTED</Button>
         <Button variant="outline-info"> WATCH TRAILER <i className='far fa-play-circle' /></Button>
       </div> 
+      </div>
+     
       <div class="ocean">
   <div class="wave"></div>
   <div class="wave"></div>
